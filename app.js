f99dbd0f3d39f7efd502901f855893cc1a58c582
Ryan Peters <ryanpeters@microsoft.com>
2026-02-24 17:57:22 -0500

    open links in a new window

--- app.js
+++ app.js
@@ -42,9 +42,17 @@ Press \`F\` for fullscreen.
 let slides = [demoMarkdown];
 let activeIndex = 0;
 
+const renderer = new marked.Renderer();
+renderer.link = function ({ href, title, tokens }) {
+  const text = this.parser.parseInline(tokens);
+  const titleAttr = title ? ` title="${title}"` : "";
+  return `<a href="${href}"${titleAttr} target="_blank" rel="noopener noreferrer">${text}</a>`;
+};
+
 marked.setOptions({
   breaks: true,
   gfm: true,
+  renderer,
 });
 
 function splitSlides(markdown) {
